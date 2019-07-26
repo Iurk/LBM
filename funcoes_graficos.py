1124cdc65090d7dc9406b10ccf9c781f93241afd
Iurk <53024617+Iurk@users.noreply.github.com>
2019-07-26 16:00:31 -0300

    Animação
    Alteração no modo de fazer a animação
    Precisa Terminar!!!

--- funcoes_graficos.py
+++ funcoes_graficos.py
@@ -5,21 +5,27 @@ Created on Tue Jul  2 13:31:44 2019
 
 @author: iurk
 """
+from images2gif import writeGif
 from matplotlib import cm
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
+from PIL import Image
 
-def grafico(u, step):
-    
+
+
+def grafico(u, step, pasta_imagens):
     file = 'vel.' + str(step) + '.png'
-    path = 'Simulacao' + '/%s' % file
+    path = pasta_imagens + '/%s' % file
     plt.clf()
     plt.imshow(u, cmap=cm.RdBu, interpolation='nearest')
     plt.colorbar(cmap=cm.RdBu)
     plt.savefig(path, dpi=250)
     
+def plot(u):
+    return plt.imshow(u, cmap='RdBu', animated=True, interpolation='nearest')
+    
 def images(u, ims):
-    ims.append([plt.imshow(u, cmap='RdBu', animated=True, interpolation='nearest')])
+    plt.clf()
+    ims.append([plot(u)])
     return ims
 
 def create_animation():
@@ -27,7 +33,19 @@ def create_animation():
     ims = []
     return fig, ims
 
-def save_animation(fig, ims):
-    writer = animation.writers['ffmpeg'](fps=60)
-    ani = animation.ArtistAnimation(fig, ims)
-    ani.save('simulation.mp4', writer=writer, dpi=500)
+def animation(fig, ims, pasta, pasta_imagens):
+    from os import listdir
+    
+    size = (1008, 1504)
+    file = 'simulation.GIF'
+    path = pasta + '/%s' % file
+    
+    files_imgs = [im for im in listdir(pasta_imagens) if im.endswith('.png')]
+    files_imgs.sort(key=__ordenar)
+    
+    images = [Image.open(pasta_imagens + '/%s' % file) for file in files_imgs]
+    writeGif(path, images, duration=0.2)
+    
+    
+def __ordenar(item):
+    return int(item.split('.')[1])
