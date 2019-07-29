f1ed488d53f736a66f75532dbae13e57cb359613
Iurk <53024617+Iurk@users.noreply.github.com>
2019-07-29 15:55:01 -0300

    Animação
    Função para geração da animação foi concluida e testada

--- funcoes_graficos.py
+++ funcoes_graficos.py
@@ -5,11 +5,11 @@ Created on Tue Jul  2 13:31:44 2019
 
 @author: iurk
 """
-from images2gif import writeGif
+#from images2gif import writeGif
 from matplotlib import cm
 import matplotlib.pyplot as plt
 from PIL import Image
-
+import imageio
 
 
 def grafico(u, step, pasta_imagens):
@@ -20,32 +20,17 @@ def grafico(u, step, pasta_imagens):
     plt.colorbar(cmap=cm.RdBu)
     plt.savefig(path, dpi=250)
     
-def plot(u):
-    return plt.imshow(u, cmap='RdBu', animated=True, interpolation='nearest')
-    
-def images(u, ims):
-    plt.clf()
-    ims.append([plot(u)])
-    return ims
-
-def create_animation():
-    fig = plt.figure()
-    ims = []
-    return fig, ims
-
-def animation(fig, ims, pasta, pasta_imagens):
+def animation(pasta, pasta_imagens):
     from os import listdir
     
-    size = (1008, 1504)
-    file = 'simulation.GIF'
+    file = 'simulation.gif'
     path = pasta + '/%s' % file
     
     files_imgs = [im for im in listdir(pasta_imagens) if im.endswith('.png')]
     files_imgs.sort(key=__ordenar)
     
-    images = [Image.open(pasta_imagens + '/%s' % file) for file in files_imgs]
-    writeGif(path, images, duration=0.2)
-    
+    images = [imageio.imread(pasta_imagens + '/%s' % file) for file in files_imgs]
+    imageio.mimsave(path, images)
     
 def __ordenar(item):
     return int(item.split('.')[1])
