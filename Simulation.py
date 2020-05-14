17176a0a88a60f300366b16e374fb4b5e1235d29
Matheus <matheusamplatz@gmail.com>
2020-05-14 14:37:12 -0300

    Att.
    Alteração do construtor para adição de novo atributo, uini, e correções para a inclusão do novo atributo. Novo método para cálculo do termo de relaxamento.

--- Simulation.py
+++ Simulation.py
@@ -5,25 +5,22 @@ Created on Thu May  7 16:54:48 2020
 
 @author: iurk
 """
-from numpy import zeros
+from numpy import float32, ones, zeros
 class Simulation():
         
-    def __init__(self, Nx, Ny, Cx, Cy, D):
+    def __init__(self, Nx, Ny, Cx, Cy, D, uini):
         self.nx = Nx
         self.ny = Ny
         self.cx = Cx
         self.cy = Cy
         self.d = D
-        
-    def gpu_parameter(self):
-        return self.nx*self.ny
+        self.uini = uini
+          
+    def initialize(self):
+        self.u = self.__init_u()
+        self.rho = self.__init_rho()
+        return self.rho, self.u
     
-    def initialize_u(self, uini):
-        self.u = zeros((2, self.nx, self.ny))
-        self.u[0, 0, :] = uini
-        
-        return self.u
-        
     def solid_create(self):
         self.solid = zeros((self.nx, self.ny), dtype=bool)
         for y in range(self.ny):
@@ -33,4 +30,20 @@ class Simulation():
                     
     def get_solid(self):
         return self.solid
+    
+    def __init_u(self):
+        u = zeros((2, self.nx, self.ny), dtype=float32)
+        u[0, 0, :] = self.uini
+        return u
+    
+    def __init_rho(self):
+        rho = ones((self.nx, self.ny), dtype=float32)
+        return rho
+    
+    def relaxation_term(self, cs, Re):
+        ni_est = (self.uini*self.d)/Re
+        self.tau = ni_est/(cs**2) + 1/2
+        self.omega = 1/self.tau
+        
+    
             
