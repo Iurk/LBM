e575c5fb9f9d63e284d555fc9b644d01ab10d313
Matheus <matheusamplatz@gmail.com>
2020-05-21 18:28:50 -0300

    Att.
    Novas declarações de funções
    * Propagation
    * Fluid Wall
    * Bounce - Back
    * Zou e He -> entrada e saída

--- GPU/gpuHeaders.cuh
+++ GPU/gpuHeaders.cuh
@@ -19,5 +19,10 @@ __global__ void approxNonEquilibrium(float* f, float* feq, float* fneq);
 __global__ void NonEquilibrium(float* tauab, float* fneq);
 __global__ void Tauab(float* tauab, float* fneq);
 __global__ void Collision(float* fout, float* feq, float* fneq);
+__global__ void Propagation(float* f, float* fout);
+__global__ void FluidWall(int* wall, int* solid);
+__global__ void BounceBack(int* fluid, int* solid, float* f, float* fout);
+__global__ void ZouHeIn(float u_inlet, float* rho, float* u, float* f);
+__global__ void ZouHeOut(int Nx, float* rho, float* u, float* f);
 
 #endif
