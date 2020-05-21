74165a6adb4da9a8b648fe3751da7ca6e4eec5d4
Matheus <matheusamplatz@gmail.com>
2020-05-21 18:28:02 -0300

    Att.
    Novas Funções:
    * Etapa de Propagação
    * Fluid Wall
    * Bounce - Back
    * Zou e He -> entrada e saída

--- GPU/gpuFunc.cu
+++ GPU/gpuFunc.cu
@@ -108,3 +108,124 @@ __global__ void Collision(float* fout, float* feq, float* fneq){
         fout[(n*h + y)*w + x] = feq[(n*h + y)*w + x] + (1 - omegad)*fneq[(n*h + y)*w + x];
     }
 }
+
+__global__ void Propagation(float* f, float* fout){
+    
+    int x = blockIdx.x*blockDim.x + threadIdx.x;
+    int y = blockIdx.y*blockDim.y + threadIdx.y;
+    int w = gridDim.x*blockDim.x;
+    int h = gridDim.y*blockDim.y;
+    
+    if(y > 0){
+        if(x > 0){
+            f[(0*h + y)*w + x] = fout[(0*h + y)*w + x];
+            f[(1*h + y)*w + x] = fout[(1*h + y)*w + (x-1)];
+            f[(2*h + y)*w + x] = fout[(2*h + (y-1))*w + x];
+            f[(3*h + y)*w + x] = fout[(3*h + y)*w + (x+1)];
+            f[(4*h + y)*w + x] = fout[(4*h + (y+1))*w + x];
+            f[(5*h + y)*w + x] = fout[(5*h + (y-1))*w + (x-1)];
+            f[(6*h + y)*w + x] = fout[(6*h + (y-1))*w + (x+1)];
+            f[(7*h + y)*w + x] = fout[(7*h + (y+1))*w + (x+1)];
+            f[(8*h + y)*w + x] = fout[(8*h + (y+1))*w + (x-1)];
+        }
+    }
+}
+
+__global__ void FluidWall(int* fluid, int* solid){
+    
+    int x = blockIdx.x*blockDim.x + threadIdx.x;
+    int y = blockIdx.y*blockDim.y + threadIdx.y;
+    int w = gridDim.x*blockDim.x;
+    
+    if(solid[y*w + x] == 1){
+        for(int n=0; n < qd; n++){
+            int x_viz = x + exd[n];
+            int y_viz = y + eyd[n];
+            if(solid[y_viz*w + x_viz] == 0){
+                fluid[y_viz*w + x_viz] = 1;
+            }
+        }
+    }
+}
+
+__global__ void BounceBack(int* fluid, int* solid, float* f, float* fout){
+    
+    int noslip[] = {0, 3, 4, 1, 2, 7, 8, 5, 6};
+    int x = blockIdx.x*blockDim.x + threadIdx.x;
+    int y = blockIdx.y*blockDim.y + threadIdx.y;
+    int w = gridDim.x*blockDim.x;
+    int h = gridDim.y*blockDim.y;
+    
+    if(fluid[y*w + x] == 1){
+        for(int n=0; n < qd; n++){
+            int x_viz = x + exd[n];
+            int y_viz = y + eyd[n];
+            if(solid[y_viz*w + x_viz] == 1){
+                int ns = noslip[n];
+                f[(ns*h + y)*w + x] = fout[(n*h + y)*w + x];
+            }
+        }
+    }
+}
+
+__global__ void ZouHeIn(float u_inlet, float* rho, float* u, float* f){
+
+    int x = blockIdx.x*blockDim.x + threadIdx.x;
+    int y = blockIdx.y*blockDim.y + threadIdx.y;
+    int w = gridDim.x*blockDim.x;
+    int h = gridDim.y*blockDim.y;
+    
+    if(x == 0){
+    
+        int idxrho = y*w + x;
+        int idxu = (0*h + y)*w + x;
+        int idx0 = (0*h + y)*w + x;
+        int idx2 = (2*h + y)*w + x;
+        int idx3 = (3*h + y)*w + x;
+        int idx4 = (4*h + y)*w + x;
+        int idx6 = (6*h + y)*w + x;
+        int idx7 = (7*h + y)*w + x;
+        
+        int idx1 = (1*h + y)*w + x;
+        int idx5 = (5*h + y)*w + x;
+        int idx8 = (8*h + y)*w + x;
+        
+        u[idxu] = u_inlet;        
+        rho[idxrho] = (f[idx0] + f[idx2] + f[idx4] + 2*(f[idx3] + f[idx6] + f[idx7]))/(1 - u[idxu]);
+        
+        f[idx1] = f[idx3] + (2/3)*rho[idxrho]*u[idxu];
+        f[idx5] = f[idx7] - (1/2)*(f[idx2] - f[idx4]) + (1/6)*rho[idxrho]*u[idxu];
+        f[idx8] = f[idx6] + (1/2)*(f[idx2] - f[idx4]) + (1/6)*rho[idxrho]*u[idxu];
+    }
+}
+
+__global__ void ZouHeOut(int Nx, float* rho, float* u, float* f){
+
+    int x = blockIdx.x*blockDim.x + threadIdx.x;
+    int y = blockIdx.y*blockDim.y + threadIdx.y;
+    int w = gridDim.x*blockDim.x;
+    int h = gridDim.y*blockDim.y;
+    
+    if(x == Nx){
+    
+        int idxrho = y*w + x;
+        int idxu = (0*h + y)*w + x;
+        int idx0 = (0*h + y)*w + x;
+        int idx1 = (1*h + y)*w + x;
+        int idx2 = (2*h + y)*w + x;
+        int idx4 = (4*h + y)*w + x;
+        int idx5 = (5*h + y)*w + x;
+        int idx8 = (8*h + y)*w + x;
+        
+        int idx3 = (3*h + y)*w + x;
+        int idx6 = (6*h + y)*w + x;
+        int idx7 = (7*h + y)*w + x;
+        
+        rho[idxrho] = 1.0;
+        u[idxu] = (f[idx0] + f[idx2] + f[idx4] + 2*(f[idx1] + f[idx5] + f[idx8]))/rho[idxrho] - 1;        
+        
+        f[idx3] = f[idx1] - (2/3)*rho[idxrho]*u[idxu];
+        f[idx6] = f[idx8] - (1/2)*(f[idx2] - f[idx4]) - (1/6)*rho[idxrho]*u[idxu];
+        f[idx7] = f[idx5] + (1/2)*(f[idx2] - f[idx4]) - (1/6)*rho[idxrho]*u[idxu];
+    }
+}
