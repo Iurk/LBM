2f476b202e91e31b26c351211174bd010cd562e3
Iurk <matheusamplatz@gmail.com>
2020-07-08 17:58:54 -0300

    Att
    Alteração do caminho da pasta Results, localizada agora na pasta bin

--- inc/paths.h
+++ inc/paths.h
@@ -1,7 +1,7 @@
 #ifndef __PATH_H
 #define __PATH_H
 
-const char folder[] = "./Results/";
+const char folder[] = "./bin/Results/";
 const char solid_mesh[] = "./bin/Mesh/mesh.bin";
 const char fluid_mesh[] = "./bin/Mesh/fluid_wall.bin";
 
