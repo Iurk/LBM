a7eff672a95ed3957587b09c090d6f1376e51dd6
Matheus <matheusamplatz@gmail.com>
2021-04-06 15:28:03 -0300

    Att
    Adicionado o folder bin

--- inc/paths.h
+++ inc/paths.h
@@ -3,6 +3,7 @@
 
 #include <string>
 
+const std::string bin_folder = "./bin/";
 const std::string folder = "./bin/Results/";
 const std::string solid_mesh = "./bin/Mesh/mesh.bin";
 const std::string fluid_mesh = "./bin/Mesh/fluid_wall.bin";
